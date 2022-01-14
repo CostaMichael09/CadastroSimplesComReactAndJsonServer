255f7bd294984ba2a1fb3715665640fb8f83a075
CostaMichael09 <76540459+CostaMichael09@users.noreply.github.com>
2022-01-14 08:13:51 -0300

    Mini projeto react

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Projeto do capítulo de React.">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Construção
+            de um cadastro desenvolvido em React!</p>
+    </Main>
